307aecc1240179320f5955268791aac12b27daf1
Murillo Sprengel Lange <lange_sp@hotmail.com>
2022-12-24 16:38:28 +0000

    dec24

---
+++ school-spring/src/main/java/br/com/murillo/schoolspring/controller/CalculatorControlelr.java
@@ -0,0 +1,26 @@
+package br.com.murillo.schoolspring.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calculadora")
+public class CalculatorControlelr {
+
+	// /calculadora/somar/10/20
+	// /calculadora/subtrair?a=100&b=39
+	
+	@GetMapping("/somar/{num}/{num2}")
+	public int	somar(@PathVariable int num, @PathVariable int num2) {
+		return (num + num2);
+	}
+	
+	@GetMapping("/subtrair")
+	public	int	subtracao(@RequestParam(name = "a") int a, @RequestParam(name = "b") int b) {
+		return (a - b);
+	}
+	
+	
+}
--- school-spring/src/main/java/br/com/murillo/schoolspring/controller/ClientController.java
+++ school-spring/src/main/java/br/com/murillo/schoolspring/controller/ClientController.java
@@ -1,7 +1,9 @@
 package br.com.murillo.schoolspring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.murillo.schoolspring.models.Client;
@@ -14,5 +16,14 @@ public class ClientController{
 	public Client obterCliente() {
 		return new Client(28, "Pedro", "123.456.789-00");
 	}
-
+	
+	@GetMapping("/{id}")
+	public Client getClientById1(@PathVariable int id) {
+		return new Client(id, "Maria", "987.654.321-00");
+	}
+	
+	@GetMapping
+	public Client getClientById2(@RequestParam(name = "id", defaultValue = "1") int id) {
+		return new Client(id, "Marcos", " 987.654.321-00");
+	}
 }
