a03b33c02ec6b179269845d2ead597dc508119c7
Murillo Sprengel Lange <lange_sp@hotmail.com>
2022-12-29 14:01:02 +0000

    backup

--- api-test/src/main/java/com/murillo/apitest/controller/ProductController.java
+++ api-test/src/main/java/com/murillo/apitest/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.murillo.apitest.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +19,7 @@ import com.murillo.apitest.model.repositories.ProductRepository;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping
 public class ProductController {
 
 	@Autowired
@@ -30,7 +32,26 @@ public class ProductController {
 		return product;
 	}
 
-	@GetMapping
+	@GetMapping("/")
+	public String produto() {
+		return "product/index";
+	}
+
+	@GetMapping(path = "name/{namePart}")
+	public Iterable<Product> getProductByName(@PathVariable String namePart) {
+		return productRep.findByNameContainingIgnoreCase(namePart);
+		//return productRep.searchByNameLike(namePart);
+	}
+	
+	@GetMapping(path = "/page/{pageNum}/{numOfElements}")
+	public Iterable<Product> getProductPages(@PathVariable(name = "pageNum") int pageNum, @PathVariable(name = "numOfElements") int numOfelements) {
+		if(numOfelements > 2)
+			numOfelements = 2;
+		Pageable page = PageRequest.of(pageNum, numOfelements);
+		return productRep.findAll(page);
+	}
+	
+	@GetMapping(path = "/all")
 	public Iterable<Product> getProducts() { // This is not a good practice, to return all table.
 		return productRep.findAll();
 	}
@@ -39,7 +60,7 @@ public class ProductController {
 	public Optional<Product> getProductById(@PathVariable int id) {
 		return productRep.findById(id);
 	}
-	
+
 	@DeleteMapping(path = "/{id}")
 	public void deleteProduct(@PathVariable int id) {
 		productRep.deleteById(id);
--- api-test/src/main/java/com/murillo/apitest/model/repositories/ProductRepository.java
+++ api-test/src/main/java/com/murillo/apitest/model/repositories/ProductRepository.java
@@ -1,9 +1,17 @@
 package com.murillo.apitest.model.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.murillo.apitest.model.entities.Product;
 
-public interface ProductRepository extends CrudRepository<Product, Integer> {
+public interface ProductRepository
+		extends CrudRepository<Product, Integer>, PagingAndSortingRepository<Product, Integer> {
 
+	public Iterable<Product> findByNameContainingIgnoreCase(String namePart);
+	
+	/*
+	 * @Query("SELECT p FROM Product p WHERE p.nome LIKE %:nome%") public
+	 * Iterable<Product> searchByNameLike(@Param("nome") String namePart);
+	 */
 }
